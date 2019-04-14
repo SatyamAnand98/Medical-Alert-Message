4b60e2698bacac8fe021858c46449cee80b07373
SatyamAnand98 <thesatemail@gmail.com>
2019-04-15 01:09:04 +0530

    2nd Day Finishing

---
+++ Female.java
@@ -0,0 +1,141 @@
+package healthcare;
+
+import java.io.BufferedReader;
+//import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+class Female
+{
+    int i;
+    String data;
+    ArrayList<String> datalist=new ArrayList<String>();
+    ArrayList<String> value=new ArrayList<String>();
+    ArrayList<Double> Value=new ArrayList<Double>();
+    double val;
+    
+    public void readfile()throws Exception
+    {
+	    BufferedReader br=new BufferedReader(new FileReader("/home/stoneduser/Desktop/Java/test.txt"));
+	    String sc;
+	    while((sc = br.readLine())!=null){
+            String[] Pair = sc.split(":");
+            datalist.add(Pair[0]);
+            value.add(Pair[1]);
+
+        }
+	    for(i=5;i<datalist.size();i++)
+	    {
+	        data=value.get(i);
+	        val=Double.parseDouble(data);
+//	        System.out.println(data);
+	        Value.add(val);
+	    }
+//	    for(i=0;i<value.size();i++)
+//	    {
+//	      vl=value.get(i);
+////	       System.out.println(vl);
+//	    }
+	    br.close();
+    }
+    public int glucose()
+    {
+            double g=Value.get(0);
+            if(g<100.0)
+                return 1;
+            else if(100.0<=g && g<=125.0)
+                return 0;
+            else
+                return 2;
+    }
+    public int calcium()
+    {
+        double c=Value.get(1);
+            if(c<8.6)
+                return 1;
+            else if(8.6<=c && c<=10.2)
+                return 0;
+            else
+                return 2;
+    }
+    public int cholesterol()
+    {
+        double ch=Value.get(2);
+            if(ch<200.0)
+                return 1;
+            else if(200.0<=ch && ch<=239.0)
+                return 0;
+            else
+                return 2;
+    }
+    public int rbc()
+    {
+        double rbc=Value.get(3);
+            if(rbc<4.1)
+                return 1;
+            else if(4.1<=rbc && rbc<=5.1)
+                return 0;
+            else
+                return 2;
+    }
+    public int wbc()
+    {
+        double wbc=Value.get(4);
+            if(wbc<4500.0)
+                return 1;
+            else if(4500.0<=wbc && wbc<=10000.0)
+                return 0;
+            else
+                return 2;
+    }
+    public int hemoglobin()
+    {
+        double h=Value.get(5);
+            if(h<12.3)
+                return 1;
+            else if(12.3<=h && h<=15.3)
+                return 0;
+            else
+                return 2;
+    }
+    public int platelets()
+    {
+        double p=Value.get(6);
+            if(p<150000.0)
+                return 1;
+            else if(150000.0<=p && p<=450000.0)
+                return 0;
+            else
+                return 2;
+    }
+    public int sodium()
+    {
+        double s=Value.get(7);
+            if(s<135.0)
+                return 1;
+            else if(135.0<=s && s<=145.0)
+                return 0;
+            else
+                return 2;
+    }
+    public int potassium()
+    {
+       double pt=Value.get(8);
+            if(pt<3.6)
+                return 1;
+            else if(3.5<=pt && pt<=5.0)
+                return 0;
+            else
+                return 2;
+    }
+    public int chloride()
+    {
+       double cl=Value.get(9);
+            if(cl<97.0)
+                return 1;
+            else if(97.0<=cl && cl<=107.0)
+                return 0;
+            else
+                return 2;
+    }
+}
--- Java_sms_requesthttp.java
+++ Java_sms_requesthttp.java
@@ -1,13 +1,108 @@
 package healthcare;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.net.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Java_sms_requesthttp {
 
         public static void main(String[] args) {
+        	String message="",PhoneNo="",Name="",gender;
+        	int Glucose,Calcium,Cholestrol,Rbc,Wbc,Hemoglobin,Platelet,Sodium,Potassium,Chloride;
+        	List<String> detailList = new ArrayList<String>();
+    	    List<String> valueList = new ArrayList<String>();
+    	    
+    	    try(BufferedReader in = new BufferedReader(new FileReader("/home/stoneduser/Desktop/Java/test.txt"))){
+    	        String line;
+    	        while((line = in.readLine())!=null){
+    	            String[] pair = line.split(":");
+    	            detailList.add(pair[0]);
+    	            valueList.add(pair[1]);
+    	        }
+    	        gender = valueList.get(1);
+//                System.out.println(gender);
+    	        Name=valueList.get(0);
+    	        PhoneNo=valueList.get(2);
+//    	        System.out.println(message);
+    	        if(gender.equals("M"))
+    	        {
+//                  System.out.println("True");
+    	          Male m=new Male();
+    	          m.readfile();
+    	          Glucose = m.glucose();
+    	          Calcium = m.calcium();
+    	          Cholestrol = m.cholesterol();
+    	          Rbc = m.rbc();
+    	          Wbc = m.wbc();
+    	          Hemoglobin = m.hemoglobin();
+    	          Platelet = m.platelets();
+    	          Sodium = m.sodium();
+    	          Potassium = m.potassium();
+    	          Chloride = m.chloride();
+    	          if(Glucose==1||Glucose==2)
+    	        	  message = message+" Glucose";
+    	          if(Calcium==1||Calcium==2)
+    	        	  message = message+" Calcium";
+    	          if(Cholestrol==1||Cholestrol==2)
+    	        	  message = message+" Cholestrol";
+    	          if(Rbc==1||Rbc==2)
+    	        	  message = message+" Rbc";
+    	          if(Wbc==1||Wbc==2)
+    	        	  message = message+" Wbc";
+    	          if(Hemoglobin==1||Hemoglobin==2)
+    	        	  message = message+" Hemoglobin";
+    	          if(Platelet==1||Platelet==2)
+    	        	  message = message+" Platelet";
+    	          if(Sodium==1||Sodium==2)
+    	        	  message = message+" Sodium";
+    	          if(Potassium==1||Potassium==2)
+    	        	  message = message+" Potassium";
+    	          if(Chloride==1||Chloride==2)
+    	        	  message = message+" Chloride";
+    	        }
+    	         else if(gender.equals("F"))
+    	         {
+    	            Female f=new Female();
+    	            f.readfile();
+    	            Glucose = f.glucose();
+    	            Calcium = f.glucose();
+    	            Cholestrol = f.cholesterol();
+    	            Rbc = f.rbc();
+    	            Wbc = f.wbc();
+    	            Hemoglobin = f.hemoglobin();
+    	            Platelet = f.platelets();
+    	            Sodium = f.sodium();
+    	            Potassium = f.potassium();
+    	            Chloride = f.chloride();
+    	            if(Glucose==1||Glucose==2)
+    	          	  message = message+" Glucose";
+    	            if(Calcium==1||Calcium==2)
+    	          	  message = message+" Calcium";
+    	            if(Cholestrol==1||Cholestrol==2)
+    	          	  message = message+" Cholestrol";
+    	            if(Rbc==1||Rbc==2)
+    	          	  message = message+" Rbc";
+    	            if(Wbc==1||Wbc==2)
+    	          	  message = message+" Wbc";
+    	            if(Hemoglobin==1||Hemoglobin==2)
+    	          	  message = message+" Hemoglobin";
+    	            if(Platelet==1||Platelet==2)
+    	          	  message = message+" Platelet";
+    	            if(Sodium==1||Sodium==2)
+    	          	  message = message+" Sodium";
+    	            if(Potassium==1||Potassium==2)
+    	          	  message = message+" Potassium";
+    	            if(Chloride==1||Chloride==2)
+    	          	  message = message+" Chloride";
+    	         }
+    	      }catch(Exception e) {
+    	    	  System.out.println("Non");
+    	      }
                 try {
-                        String recipient = "+918240092248";
-                        String message = "Hello World";
+                        String recipient = PhoneNo;
+                        message = "Dear "+Name+" you have critical situation regarding"+message+" Please visit Doctor ASAP!!";
                         String username = "admin";
                         String password = "741852963";
                         String originator = "+919513868175";
@@ -17,12 +112,13 @@ public class Java_sms_requesthttp {
             "&password=" + URLEncoder.encode(password, "UTF-8") +
             "&recipient=" + URLEncoder.encode(recipient, "UTF-8") +
             "&messagetype=SMS:TEXT" +
-            "&messagedata=" + URLEncoder.encode(message, "UTF-8") +
+            "&messagedata=" + URLEncoder.encode(message , "UTF-8") +
             "&originator=" + URLEncoder.encode(originator, "UTF-8") +
             "&serviceprovider=GSMModem1" +
             "&responseformat=html";
                         
-                        System.out.println(requestUrl);
+//                        System.out.println(requestUrl);
+//                        System.out.println(message);
 
 
 
